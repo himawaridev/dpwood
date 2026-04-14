430bc519cc22e030fb00f97d34b32ab4fd301736
himawaridev <dunghoang22102003@gmail.com>
2026-04-14 19:01:03 +0700

    update docker

--- server/src/config/connectSequelize.js
+++ server/src/config/connectSequelize.js
@@ -2,9 +2,9 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: 3306,
+    host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
     logging: false,
 });
 
